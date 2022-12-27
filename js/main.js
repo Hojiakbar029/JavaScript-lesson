7b27e447333728797c91102a97044cddd0f9a456
unknown <hergashev727@gmail.com>
2022-12-27 23:33:08 +0300

    JavaScript lesson

---
+++ js/main.js
@@ -0,0 +1,29 @@
+alert("Assalomu Alekum 'JavaScriptga' Hush Kelibsiz")
+
+var Name = prompt('Ismingizni kiriting!')
+
+console.log(Name);
+
+var surname = prompt('Familyangizni kiriting!')
+
+console.log(surname);
+
+var place = prompt('Yashash manzilingizni kiriting!')
+
+console.log(place);
+
+var number1 = prompt('Uyingizni raqamini kiriting!')
+
+console.log(number1);
+
+var number2 = prompt('Telefon raqamingizni kiriritng!')
+
+console.log(number2);
+
+var information = prompt("JavaScript haqida ma'lumot bilasizmi?")
+
+console.log(information);
+
+var differnce = prompt('Low-Level bilan High-Level ni farqini bilasizmi?')
+
+console.log(differnce);
